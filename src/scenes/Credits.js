f2a111b211f5a73e255d0e5d8dce093b30f2d314
Bryon Anderson <brkander@ucsc.edu>
2024-03-18 23:06:41 -0700

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -12,8 +12,10 @@ class Credits extends Phaser.Scene {
         let title01 = this.add.bitmapText(centerX, game.config.height/10, '8-bit', 'Credits', 50).setOrigin(0.5).setTint(0xe28e18);
         this.add.bitmapText(centerX, game.config.height/5, '8-bit', 'Thank you so much for playing!', 20).setOrigin(0.5);
         this.add.bitmapText(centerX, game.config.height/3.5, '8-bit', 'Game design by: Bryon Anderson', 20).setOrigin(0.5);
-        this.add.bitmapText(centerX- 30, game.config.height/2, '8-bit','Font created by:', 20).setOrigin(0.5);
-        this.add.bitmapText(centerX- 20, game.config.height/1.5, '8-bit', 'Tilemap Assets created by: Anokolisa https://anokolisa.itch.io/', 17).setOrigin(0.5);
+        this.add.bitmapText(centerX, game.config.height/2, '8-bit','Font created by: Craftron Gaming', 20).setOrigin(0.5);
+        this.add.bitmapText(centerX, game.config.height/2.5, '8-bit','Sounds sourced from freesound.org ', 20).setOrigin(0.5);
+
+        this.add.bitmapText(centerX, game.config.height/1.5, '8-bit', 'Tilemap Assets created by: Anokolisa https://anokolisa.itch.io/', 20).setOrigin(0.5);
        
         
         this.add.bitmapText(centerX, h - textSpacer, '8-bit', 'Up arrow to Guide', 16).setOrigin(0.5);
